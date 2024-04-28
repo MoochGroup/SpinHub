f07d9c0733be00393efa8a217d93e63d80ec97b2
MZakary <zakary-mitrovic@hotmail.ca>
2024-04-28 19:23:24 -0400

    added some hovercards and popover

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -3,6 +3,16 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/Components/ui/popover"
+import { Button } from "@/Components/ui/button"
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/Components/ui/hover-card"
+import { CalendarDays } from "lucide-react"
+import { Avatar, AvatarFallback, AvatarImage } from "@/Components/ui/avatar"
+
+
 
 function Header() {
 
@@ -15,62 +25,54 @@ function Header() {
     </div>
 
     <div className="Icon__Container">
-      <div className="Account__Icon"><span className="material-icons">account_circle</span></div>
-      <div className="Account__Icon"><span className="material-icons">settings</span></div>
-      <div className="Account__Icon"><span className="material-icons">translate</span></div>
+      <div className="Icon"><span className="material-icons">settings</span></div>
+      <div className="Account__Button">
+      <HoverCard>
+        <HoverCardTrigger asChild>
+        <div className="Icon"><span className="material-icons">account_circle</span></div>
+        </HoverCardTrigger>
+        <HoverCardContent className="w-80">
+          <div className="flex justify-between space-x-4">
+            <Avatar>
+              <AvatarImage src="/Img/ProfileTest/Bobby.png" />
+              <AvatarFallback>VC</AvatarFallback>
+            </Avatar>
+            <div className="space-y-1">
+              <h4 className="text-sm font-semibold">@Bob</h4>
+              <p className="text-sm">
+              Bob, the eccentric inventor with a penchant for speaking in rhymes.
+              </p>
+              <div className="flex items-center pt-2">
+                <CalendarDays className="mr-2 h-4 w-4 opacity-70" />{" "}
+                <span className="text-xs text-muted-foreground">
+                  Joined December 2021
+                </span>
+              </div>
+            </div>
+          </div>
+        </HoverCardContent>
+      </HoverCard>
+      </div>
+      <div className="Translate__Button">  
+        <Popover>
+          <PopoverTrigger asChild>
+          <div className="Icon"><span className="material-icons">translate</span></div>
+          </PopoverTrigger>
+          <PopoverContent className="w-80 grid gap-4">
+            <div className="space-y-2">
+              <h4 className="font-medium leading-none">Language</h4>
+              <p className="text-sm text-muted-foreground">
+                Set the language for the site.
+              </p>
+            </div>
+            <Button variant="outline">English</Button>
+            <Button variant="outline">French</Button>
+          </PopoverContent>
+        </Popover>
+      </div>
+
     </div>
 
-
-    <Popover>
-      <PopoverTrigger asChild>
-        {/* <Button variant="outline">Open popover</Button> */}
-        <button>open</button>
-      </PopoverTrigger>
-      <PopoverContent className="w-80">
-        <div className="grid gap-4">
-          <div className="space-y-2">
-            <h4 className="font-medium leading-none">Dimensions</h4>
-            <p className="text-sm text-muted-foreground">
-              Set the dimensions for the layer.
-            </p>
-          </div>
-          <div className="grid gap-2">
-            <div className="grid grid-cols-3 items-center gap-4">
-              {/* <Label htmlFor="width">Width</Label>
-              <Input
-                id="width"
-                defaultValue="100%"
-                className="col-span-2 h-8"
-              /> */}
-            </div>
-            <div className="grid grid-cols-3 items-center gap-4">
-              {/* <Label htmlFor="maxWidth">Max. width</Label>
-              <Input
-                id="maxWidth"
-                defaultValue="300px"
-                className="col-span-2 h-8"
-              /> */}
-            </div>
-            <div className="grid grid-cols-3 items-center gap-4">
-              {/* <Label htmlFor="height">Height</Label>
-              <Input
-                id="height"
-                defaultValue="25px"
-                className="col-span-2 h-8"
-              /> */}
-            </div>
-            <div className="grid grid-cols-3 items-center gap-4">
-              {/* <Label htmlFor="maxHeight">Max. height</Label>
-              <Input
-                id="maxHeight"
-                defaultValue="none"
-                className="col-span-2 h-8"
-              /> */}
-            </div>
-          </div>
-        </div>
-      </PopoverContent>
-    </Popover>
     
   </section>
   );
