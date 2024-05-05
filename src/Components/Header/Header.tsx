eee79d36a07195bf0124b9d2068cf078760f7d09
Marcus <m-a.boulet.pro@hotmail.com>
2024-05-05 16:44:23 -0400

    Zakus prend ceci courcombre

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -14,62 +14,72 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/Components/ui/avatar";
 
 function Header() {
   return (
-    <div className="Sidebar">
-      <div className="Sidebar__Content">
-        <div className="Icon">
-          <span className="material-icons">settings</span>
-        </div>
-        <div className="Account__Button">
-          <HoverCard>
-            <HoverCardTrigger asChild>
-              <div className="Icon">
-                <span className="material-icons">account_circle</span>
-              </div>
-            </HoverCardTrigger>
-            <HoverCardContent className="w-80">
-              <div className="flex justify-between space-x-4">
-                <Avatar>
-                  <AvatarImage src="/Img/ProfileTest/Bobby.png" />
-                  <AvatarFallback>VC</AvatarFallback>
-                </Avatar>
-                <div className="space-y-1">
-                  <h4 className="text-sm font-semibold">@Bob</h4>
-                  <p className="text-sm">
-                    Bob, the eccentric inventor with a penchant for speaking in
-                    rhymes.
-                  </p>
-                  <div className="flex items-center pt-2">
-                    <CalendarDays className="mr-2 h-4 w-4 opacity-70" />{" "}
-                    <span className="text-xs text-muted-foreground">
-                      Joined December 2021
-                    </span>
+    <>
+      <div className="Title__Div">
+        <h1 className="SpinHub__Title animate__animated animate__zoomInUp">
+          Welcome to SpinHub!
+        </h1>
+        <p className="SpinHub__Title__Description animate__animated animate__fadeInDown animate__delay-1s">
+          Discover Random Delights with Every Spin!
+        </p>
+      </div>
+      <div className="Sidebar">
+        <div className="Sidebar__Content">
+          <div className="Icon">
+            <span className="material-icons">settings</span>
+          </div>
+          <div className="Account__Button">
+            <HoverCard>
+              <HoverCardTrigger asChild>
+                <div className="Icon">
+                  <span className="material-icons">account_circle</span>
+                </div>
+              </HoverCardTrigger>
+              <HoverCardContent className="w-80">
+                <div className="flex justify-between space-x-4">
+                  <Avatar>
+                    <AvatarImage src="/Img/ProfileTest/Bobby.png" />
+                    <AvatarFallback>VC</AvatarFallback>
+                  </Avatar>
+                  <div className="space-y-1">
+                    <h4 className="text-sm font-semibold">@Bob</h4>
+                    <p className="text-sm">
+                      Bob, the eccentric inventor with a penchant for speaking
+                      in rhymes.
+                    </p>
+                    <div className="flex items-center pt-2">
+                      <CalendarDays className="mr-2 h-4 w-4 opacity-70" />{" "}
+                      <span className="text-xs text-muted-foreground">
+                        Joined December 2021
+                      </span>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </HoverCardContent>
-          </HoverCard>
-        </div>
-        <div className="Translate__Button">
-          <Popover>
-            <PopoverTrigger asChild>
-              <div className="Icon">
-                <span className="material-icons">translate</span>
-              </div>
-            </PopoverTrigger>
-            <PopoverContent className="w-80 grid gap-4">
-              <div className="space-y-2">
-                <h4 className="font-medium leading-none">Language</h4>
-                <p className="text-sm text-muted-foreground">
-                  Set the language for the site.
-                </p>
-              </div>
-              <Button variant="outline">English</Button>
-              <Button variant="outline">French</Button>
-            </PopoverContent>
-          </Popover>
+              </HoverCardContent>
+            </HoverCard>
+          </div>
+          <div className="Translate__Button">
+            <Popover>
+              <PopoverTrigger asChild>
+                <div className="Icon">
+                  <span className="material-icons">translate</span>
+                </div>
+              </PopoverTrigger>
+              <PopoverContent className="w-80 grid gap-4">
+                <div className="space-y-2">
+                  <h4 className="font-medium leading-none">Language</h4>
+                  <p className="text-sm text-muted-foreground">
+                    Set the language for the site.
+                  </p>
+                </div>
+                <Button variant="outline">English</Button>
+                <Button variant="outline">French</Button>
+              </PopoverContent>
+            </Popover>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
